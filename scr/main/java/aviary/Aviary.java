0d8f13798c4f8f337597b06e3b9d739edac61394
Dmitrii Prikhozhaev <prigho-dima@ya.ru>
2020-07-01 14:43:05 +0400

    release Zoo app

--- scr/main/java/aviary/Aviary.java
+++ scr/main/java/aviary/Aviary.java
@@ -3,15 +3,9 @@ package aviary;
 import animal.Animal;
 
 public abstract class Aviary {
-    private int maxCountOfAnimals;
-    private int animalsCount;
-    private int aviarySize = 0;
-
     public abstract void addAnimal(Animal animal);
 
     public abstract int getCount();
 
     public abstract Animal getAnimalNumber(int i);
-
-
 }
