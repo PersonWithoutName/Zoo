6627b4f94151b12a05f32f0634c4100cb6dc6e41
Dmitrii Prikhozhaev <prigho-dima@ya.ru>
2020-07-08 16:45:40 +0400

    added log4j dependency and maven compile plugin

--- src/main/java/animal/Herbivore.java
+++ src/main/java/animal/Herbivore.java
@@ -2,20 +2,26 @@ package animal;
 
 import food.AllFood;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public abstract class Herbivore extends Animal {
+
     Herbivore(String name) {
         super(name);
     }
 
+    private static final Logger logger = LogManager.getLogger();
+
     @Override
     public void eat(AllFood allFood) {
-        if (allFood == AllFood.GRASS || allFood == AllFood.VEGETABLES){
+        if (allFood == AllFood.GRASS || allFood == AllFood.VEGETABLES) {
             System.out.println(getName() + " eating " + allFood.getFoodName());
         } else {
             try {
                 throw new WrongFoodException("WrongFood");
             } catch (Exception e) {
-                System.err.println(getName() + " don't eat this food");
+                logger.error(getName() + " don't eat this food");
             }
         }
     }
